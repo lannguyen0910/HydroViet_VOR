15d39cadc3d9684191ea56630e3ca3d15d8c4955
lan.nguyen2k <47696901+lannguyen0910@users.noreply.github.com>
2020-10-18 22:34:10 +0700

    update datasets

--- datasets.py
+++ datasets.py
@@ -4,7 +4,61 @@ import torch.utils.data as Data
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 from PIL import Image
+
+
+class TextClassificationDataset(Data.Dataset):
+    """
+    Load a dataset.csv with input of (category, text)
+    """
+
+    def __init__(self, root,
+                 tokenizer=str.split(''),
+                 n_samples=None,
+                 shuffle=False,
+                 skip_header=True
+                 ):
+        self.root = root
+        self.n_samples = n_samples
+        self.tokenizer = tokenizer
+        self.shuffle = shuffle
+        self.skip_header = skip_header
+        self.data = self.load_data()[0]
+        self.n_categories = self.load_data()[1]
+
+    def load_data(self):
+        data = []
+        with open(self.root, 'r', encoding='utf8') as csv_file:
+            reader = csv.reader(csv_file)
+            if self.skip_header:
+                next(reader)
+            for line in reader:
+                data.append(line)
+
+        if self.shuffle:
+            random.shuffle(data)
+
+        data = data[:self.n_samples] if self.n_samples is not None \
+            and self.n_samples <= len(data) else data
+
+        # First Column -> Categories , Second Column -> Text
+        n_categories = list(set([line[0] for line in data]))
+        return (data, n_categories)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        category, text = self.data[idx]
+        tokens = self.tokenizer(text)
+        return {'text': tokens, 'category': category}
+
+    def __str__(self):
+        title = 'Dataset for Text Classification\n\n'
+        samples = f'Number of samples: {len(self.data)}\n'
+        classes = f'Number of classes: {len(self.n_categories)}\n'
+        return title + samples + classes
 
 
 class ImageClassificationDataset(Data.Dataset):
@@ -63,7 +117,7 @@ class ImageClassificationDataset(Data.Dataset):
 
         if 'freqs' in types:
             barplot = plt.bar(list(count_dict.keys()), list(count_dict.values()), color=[
-                              np.random.rand(3,) for _ in range(len(self.n_classes))])
+                np.random.rand(3,) for _ in range(len(self.n_classes))])
             for rect_per_class in barplot:
                 height = rect_per_class.get_height()
                 plt.text(rect_per_class.get_x() + rect_per_class.get_width()/2.0, '%d' %
@@ -86,7 +140,7 @@ class ImageClassificationDataset(Data.Dataset):
                 img = self.transforms('train', img)['train']
             elif mode == 'val':
                 img = self.transforms('val', img)['val']
-            elif mode == 'val':
+            elif mode == 'test':
                 img = self.transforms('test', img)['test']
             else:
                 print('Error! Please rename your folder')
@@ -96,7 +150,6 @@ class ImageClassificationDataset(Data.Dataset):
 
     def __str__(self):
         title = 'Dataset for Image Classification\n\n'
-        root = f'Root: {self.root}\n'
         samples = f'Number of samples: {len(self.data)}\n'
         classes = f'Number of classes: {len(self.n_classes)}\n'
-        return title + root + samples + classes
+        return title + samples + classes
