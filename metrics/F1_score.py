cc2dfa826c33da85143b6c4bc58433459b1b1589
lan.nguyen2k <47696901+lannguyen0910@users.noreply.github.com>
2020-10-29 13:15:48 +0700

    update F1_score

--- metrics/F1_score.py
+++ metrics/F1_score.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class ClassificationF1Score:
@@ -9,14 +11,23 @@ class ClassificationF1Score:
         self.reset()
 
     def update(self, outputs, targets):
+        print('Output: ', outputs)
+        print('Target: ', targets)
         pred = torch.argmax(outputs, 1)
+        print('Output after argmax: ', pred)
         pred = pred.cpu().numpy()
         target = targets.cpu().numpy()
+        print('Target shape: ', target.shape)
+
         for pd, gt in zip(pred, target):
+            print('Pd: ', pd)
+            print('Gt: ', gt)
             self.count_dict[gt]['total_gt'] += 1
             self.count_dict[pd]['total_p'] += 1
             if pd == gt:
                 self.count_dict[pd]['total_pt'] += 1
+
+        print(self.count_dict)
 
     def compute(self, item, epsilon=1e-7):
         try:
@@ -71,6 +82,47 @@ class ClassificationF1Score:
 
     def __str__(self) -> str:
         return f'F1-score: {self.value()}'
+
+
+class F1_Loss(nn.Module):
+    '''Calculate F1 score. Can work with gpu tensors
+
+    The original implmentation is written by Michal Haltuf on Kaggle.
+
+    Returns
+    -------
+    torch.Tensor
+        `ndim` == 1. epsilon <= val <= 1
+
+    Reference
+    ---------
+    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
+    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
+    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
+    - http://www.ryanzhang.info/python/writing-your-own-loss-function-module-for-pytorch/
+    '''
+
+    def __init__(self, epsilon=1e-7):
+        super().__init__()
+        self.epsilon = epsilon
+
+    def forward(self, y_pred, y_true,):
+        assert y_pred.ndim == 2
+        assert y_true.ndim == 1
+        y_true = F.one_hot(y_true, 2).to(torch.float32)
+        y_pred = F.softmax(y_pred, dim=1)
+
+        tp = (y_true * y_pred).sum(dim=0).to(torch.float32)
+        tn = ((1 - y_true) * (1 - y_pred)).sum(dim=0).to(torch.float32)
+        fp = ((1 - y_true) * y_pred).sum(dim=0).to(torch.float32)
+        fn = (y_true * (1 - y_pred)).sum(dim=0).to(torch.float32)
+
+        precision = tp / (tp + fp + self.epsilon)
+        recall = tp / (tp + fn + self.epsilon)
+
+        f1 = 2 * (precision*recall) / (precision + recall + self.epsilon)
+        f1 = f1.clamp(min=self.epsilon, max=1-self.epsilon)
+        return 1 - f1.mean()
 
 
 if __name__ == '__main__':
--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,2 +1,2 @@
 from .classification_accuracy import ClassificationAccuracyMetric
-from .F1_score import ClassificationF1Score
+from .F1_score import ClassificationF1Score, F1_Loss
