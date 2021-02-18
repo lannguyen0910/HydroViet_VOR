cce8ba3b48905de5b578c31c91995c7082045022
lannguyen <18120051@student.hcmus.edu.vn>
2021-02-18 23:00:57 +0700

    add Triplet dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,3 +3,4 @@ from .text_classification import TextClassificationDataset
 from .transform import transforming
 from .vocabulary import VocabularyDataset
 from .object_detection import ObjectDetectionDataset
+from .object_retrieval.triplet import TripletDataset
