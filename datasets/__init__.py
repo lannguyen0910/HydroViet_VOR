ecc845930c082a8e47be7a494a45414eb634767c
lannguyen <18120051@student.hcmus.edu.vn>
2021-04-06 16:14:37 +0700

    update

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,2 @@
 from .image_classification import ImageClassificationDataset
-from .text_classification import TextClassificationDataset
-from .vocabulary import VocabularyDataset
-# from .object_detection import ObjectDetectionDataset
-# from .object_retrieval.triplet_MNIST import TripletDataset
 from .object_retrieval.triplet import TripletDataset
--- losses/__init__.py
+++ losses/__init__.py
@@ -1,6 +1,6 @@
 from .focal_loss import FocalLoss3, FocalLoss1, FocalLoss2
 from .smoothingCE import SmoothingCE
 # from .multibox_loss import MultiBoxLoss, multibox_loss
-from .object_retrieval.constractive_loss import ContrastiveLoss, OnlineContrastiveLoss
+from .object_retrieval.contrastive_loss import ContrastiveLoss, OnlineContrastiveLoss
 from .object_retrieval.triplet_loss import TripletLoss, OnlineTripletLoss, OnlineTripletWithClsLoss
 from .object_retrieval.supcon_loss import SupConLoss
